1a0074201ad46f87f9ac1db41a1bc2e64a17cb5e
Dimitar <dvitanov@bitdms.com>
2026-06-29 00:41:51 +0200

    Changing cron

--- routes/console.php
+++ routes/console.php
@@ -14,6 +14,6 @@ Schedule::command('queue:work --stop-when-empty --max-time=55')
     ->runInBackground();
 
 Schedule::command('app:verify-agency-domains')
-    ->everyThirtyMinutes()
+    ->everyMinute()
     ->withoutOverlapping()
     ->runInBackground();

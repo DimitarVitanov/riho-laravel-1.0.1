bf1183a9d2fd7c15d1f95a6fa792ad57c50da9d2
Dimitar <dvitanov@bitdms.com>
2026-06-28 22:46:06 +0200

    Add Support Tickets link to waitlist sidebar

--- resources/views/layouts/simple/sidebar-villabit.blade.php
+++ resources/views/layouts/simple/sidebar-villabit.blade.php
@@ -33,6 +33,14 @@
                     </a>
                 </li>
 
+                <li class="sidebar-list"><i class="fa fa-thumb-tack"></i>
+                    <a class="sidebar-link sidebar-title link-nav menu-link" href="{{ route('agency.support.index') }}">
+                        <svg class="stroke-icon"><use href="{{ asset('assets/svg/icon-sprite.svg#stroke-others') }}"></use></svg>
+                        <svg class="fill-icon"><use href="{{ asset('assets/svg/icon-sprite.svg#fill-others') }}"></use></svg>
+                        <span>Support Tickets</span>
+                    </a>
+                </li>
+
                 {{-- ===================== ADMIN / SUPER ADMIN SIDEBAR ===================== --}}
                 @elseif($user && $user->isAdmin())
                 <li class="sidebar-main-title"><div><h6>Admin Panel</h6></div></li>
